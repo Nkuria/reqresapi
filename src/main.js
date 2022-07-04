2e067d6fa374235b9c8c01f4a1e6a509de853743
Nkuria <nkuria01@gmail.com>
2022-07-04 09:45:18 +0300

    changed vue router routes

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+Vue.config.productionTip = false
  
 import 'bootstrap/dist/css/bootstrap.min.css'
+import '@/assets/css/main.css'
 
 // createApp(App).mount('#app')
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,57 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// import { createRouter, createWebHistory } from 'vue-router'
+// import HomeView from '../views/HomeView.vue'
 
-const routes = [
+// const routes = [
+//   {
+//     path: '/',
+//     name: 'home',
+//     component: HomeView
+//   },
+//   {
+//     path: '/about',
+//     name: 'about',
+//     // route level code-splitting
+//     // this generates a separate chunk (about.[hash].js) for this route
+//     // which is lazy-loaded when the route is visited.
+//     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+//   }
+// ]
+
+// const router = createRouter({
+//   history: createWebHistory(process.env.BASE_URL),
+//   routes
+// })
+
+// export default router
+
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+ 
+Vue.use(VueRouter)
+ 
+  const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'signup',
+    component: () => import('../components/userRegistration.vue')
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/login',
+    name: 'login',
+    component: () => import('../components/userLogin.vue')
+  },
+  {
+    path: '/forgot-password',
+    name: 'forgot-password',
+    component: () => import('../components/ForgotPassword.vue')
   }
 ]
-
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+ 
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes
 })
-
+ 
 export default router
