fda8683efea494bbbf7bf4cda6ec88892f908b51
Nkuria <nkuria01@gmail.com>
2022-07-04 09:26:11 +0300

    Created vue app, login, registration and forgot password pages

---
+++ src/main.js
@@ -0,0 +1,12 @@
+// import { createApp } from 'vue'
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+ 
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+// createApp(App).mount('#app')
+new Vue({
+    router,
+    render: h => h(App)
+  }).$mount('#app')
